62d138828ca284fc2d4fd14ff32d7768d82bd548
basit ali <basitaali37@gmail.com>
2025-08-28 22:53:09 +0500

    Landing page done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import { FaShoppingCart , FaBars , FaTimes } from "react-icons/fa"
 
 const Header = () => {
 
-    const [ isMenuSelected , setIsMenuSelected ] = useState('False');
+    const [ isMenuSelected , setIsMenuSelected ] = useState('True');
 
     function changeMenuState(){
         setIsMenuSelected(!isMenuSelected);
@@ -14,7 +14,7 @@ const Header = () => {
 
   <>
 
-    <div className='bg-[#E3E6F3] drop-shadow-lg px-18 flex justify-between items-center w-full h-20 fixed top-0 left-0 z-50'>
+    <div className='bg-[#E3E6F3] drop-shadow-lg px-6 md:px-18 flex justify-between items-center w-full h-20 fixed top-0 left-0 z-50'>
 
       <div className="">
         <img src="src\assets\images\logo.png" alt="logo image" />  
@@ -30,21 +30,22 @@ const Header = () => {
       </div>
 
       <div onClick = { changeMenuState } className='block md:hidden'>
-        { isMenuSelected ? <FaTimes className='text-3xl text-[rgba(0,0,0,0.8)]'/> : <FaBars className='text-3xl text-[rgba(0,0,0,0.8)]'/> }
+        { isMenuSelected ? <FaBars className='text-3xl text-[rgba(0,0,0,0.8)]'/> : <FaTimes className='text-3xl text-[rgba(0,0,0,0.8)]'/> }
       </div>
 
       
     </div>
 
-    { isMenuSelected && 
+    { !isMenuSelected && 
       
-      <div className = " md:hidden absolute right-0 top-20 flex flex-col items-center justify-between py-10 bg-[#E3E6F3] h-[450px] w-[60vw] rounded-xl shadow-lg transition-all ease-in-out duration-500 " >
+      <div className = " md:hidden absolute right-0 top-20 flex flex-col items-center justify-between py-10 bg-[#E3E6F3] h-[450px] w-[60vw] rounded-xl shadow-lg transition-all ease-in-out duration-500 z-40" >
 
         <a className="text-2xl text-[#1a1a1a] hover:text-[#088178]" href="#">Home</a>
-        <a className="text-2xl mt-10 text-[#1a1a1a] hover:text-[#088178]" href="#">Shop</a>
-        <a className="text-2xl mt-10 text-[#1a1a1a] hover:text-[#088178]" href="#">Blog</a>
-        <a className="text-2xl mt-10 text-[#1a1a1a] hover:text-[#088178]" href="#">About</a>
-        <a className="text-2xl mt-10 text-[#1a1a1a] hover:text-[#088178]" href="#">Content</a>
+        <a className="text-2xl mt-8 text-[#1a1a1a] hover:text-[#088178]" href="#">Shop</a>
+        <a className="text-2xl mt-8 text-[#1a1a1a] hover:text-[#088178]" href="#">Blog</a>
+        <a className="text-2xl mt-8 text-[#1a1a1a] hover:text-[#088178]" href="#">Cart</a>
+        <a className="text-2xl mt-8 text-[#1a1a1a] hover:text-[#088178]" href="#">About</a>
+        <a className="text-2xl mt-8 text-[#1a1a1a] hover:text-[#088178]" href="#">Content</a>
           
       </div>
 
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,12 +2,29 @@ import React from 'react'
 
 const Hero = () => {
   return (
+
+<>
+
  <div 
-  className="relative overflow-hidden h-[100vh] w-full bg-cover bg-[center_top_25px]"
-  style={{ backgroundImage: "url('src/assets/images/hero4.png')" }}
->
+  className="overflow-hidden h-[100vh] w-full bg-cover bg-[center_top_25px]"
+  style={{ backgroundImage: "url('src/assets/images/hero4.png')" }}>
+
+    <h3 className='text-gray-700 text-2xl md:text-3xl absolute top-70 left-10 md:top-90 md:left-30 font-semibold'>Trade-in-offer</h3>
+    <h1 className='text-black text-4xl md:text-6xl absolute top-80 left-10 md:top-105 md:left-30 font-bold tracking-wider'>
+      Super value deals <br/>
+      <span className='text-[#088178]'> On all products </span>
+    </h1>
+
+    <p className='text-gray-500 text-xl font-semibold absolute top-105 left-10 md:top-140 md:left-30'>Save more with coupons & 70% off</p>
+
+    <button className="cursor-pointer w-60 h-12.5 font-bold text-[#088178] text-xl absolute top-120 left-10 md:top-157 md:left-42" 
+    style ={{ backgroundImage: "url('src/assets/images/button.png')"}}>
+      Shop Now
+    </button>
+
 </div>
 
+</>
 
   )
 }
