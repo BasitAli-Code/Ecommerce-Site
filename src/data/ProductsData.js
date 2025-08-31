4c5e958023dc90cfbfd254b61090f6a874fb2a1f
basit ali <basitaali37@gmail.com>
2025-08-31 11:53:36 +0500

    Created Reusable Component

---
+++ src/data/ProductsData.js
@@ -0,0 +1,23 @@
+  const ProductImages = [
+    "src/assets/images/f1.jpg",
+    "src/assets/images/f2.jpg",
+    "src/assets/images/f3.jpg",
+    "src/assets/images/f4.jpg",
+    "src/assets/images/f5.jpg",
+    "src/assets/images/f6.jpg",
+    "src/assets/images/f7.jpg",
+    "src/assets/images/f8.jpg",
+  ]
+
+  const ArrivalImages = [
+    "src/assets/images/n1.jpg",
+    "src/assets/images/n2.jpg",
+    "src/assets/images/n3.jpg",
+    "src/assets/images/n4.jpg",
+    "src/assets/images/n5.jpg",
+    "src/assets/images/n6.jpg",
+    "src/assets/images/n7.jpg",
+    "src/assets/images/n8.jpg",
+  ]
+
+  export { ProductImages , ArrivalImages }
